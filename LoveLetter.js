2a9fa3d88ee59e4a439fbcc3ae63d7769f7da76b
varunboga <varunboga2005@gmail.com>
2024-08-18 12:53:40 +0530

    Add files via upload

---
+++ LoveLetter.js
@@ -0,0 +1,48 @@
+import React, { useState, useRef } from 'react';
+import './LoveLetter.css';
+import audioFile from './Ninnila.mp3';
+
+const LoveLetter = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isFullSize, setIsFullSize] = useState(false);
+  const audioRef = useRef(null);
+
+  const handleOpenLetter = () => {
+    setIsOpen(true);
+    setTimeout(() => {
+      setIsFullSize(true);
+      // Ensuring audio play is directly a result of this user interaction
+      if (audioRef.current) {
+        audioRef.current.play()
+          .then(() => console.log("Playback succeeded"))
+          .catch(e => console.error("Playback failed:", e));
+      }
+    }, 800);
+  };
+
+  const handleCloseLetter = () => {
+    setIsFullSize(false);
+    setTimeout(() => {
+      if (audioRef.current) {
+        audioRef.current.pause();
+      }
+      setIsOpen(false);
+    }, 800);
+  };
+
+  return (
+    <div className={`envelope ${isOpen ? 'open' : ''}`} onClick={!isFullSize ? handleOpenLetter : handleCloseLetter}>
+      <div className="flap"></div>
+      <div className="body"></div>
+      <div className={`letter ${isFullSize ? 'fullSize' : ''}`}>
+        Hiiiiiii Bingoooo🖤,<br />
+        Wishh youu a veryy HAPPYY HAPPPYY HAPPIESTT BIRTHDAYYY!!<br />May thiss bday bringss uu all joyy and happinesss)<br />
+        I amm veryy luckyy to havee youu inn myy lifee bangarammm!!,<br />
+        @tommm
+      </div>
+      <audio ref={audioRef} src={audioFile} onError={(e) => console.error('Audio error:', e.message)} />
+    </div>
+  );
+};
+
+export default LoveLetter;
